da9d36708baaa4519f4dd42cf49db499350135bc
juarodfer3 <juarodfer3@alu.edu.gva.es>
2026-02-23 11:06:37 +0100

    main

--- UD6/src/ac4/Main.java
+++ UD6/src/ac4/Main.java
@@ -61,6 +61,43 @@ public class Main {
         // iii.) Crea un objeto CuerpoCeleste de tipo Luna y periodo orbital 27, agrégalo al
         // sistemaSolar y añade al objeto LaTierra que creamos en el apartado anterior
         // este satélite.
-        
+        CuerpoCeleste Luna = new Luna("Luna", 27);
+        sistemaSolar.put("Luna", Luna);
+        LaTierra.addSatelite(Luna);
+
+        // iv.) Crea otras dos lunas más (Deimos-1,3 y Phobos-0,3) añádelas al sistema solar y
+        // agrégalas también en el planeta Marte como satélites.
+        CuerpoCeleste deimos = new Luna("Deimos", 1.3);
+        CuerpoCeleste phobos = new Luna("Phobos", 0.3);
+        sistemaSolar.put("Deimos", deimos);
+        sistemaSolar.put("Phobos", phobos);
+        marte.addSatelite(deimos);
+        marte.addSatelite(phobos);
+
+        // v.) Crea otras cuatro lunas más (Io-1,8 Europa-3,5, Ganymede-7,1 Callisto-16.7)
+        // añádelas al sistema solar y agrégalas también en el planeta Júpiter como
+        // satélites.
+        CuerpoCeleste lo = new Luna("lo", 1.8);
+        CuerpoCeleste europa = new Luna("Europa", 3.5);
+        CuerpoCeleste ganymede = new Luna("Ganymede", 7.1);
+        CuerpoCeleste callisto = new Luna("Callisto", 16.7);
+        sistemaSolar.put("lo", lo);
+        sistemaSolar.put("Europa", europa);
+        sistemaSolar.put("Ganymede", ganymede);
+        sistemaSolar.put("Callisto", callisto);
+        jupiter.addSatelite(lo);
+        jupiter.addSatelite(europa);
+        jupiter.addSatelite(ganymede);
+        jupiter.addSatelite(callisto);
+
+        // vi.) Con un for-each imprime el nombre de todos los planetas que hemos
+        // almacenado en el conjunto (Set).
+        for (CuerpoCeleste x : sistemaSolar.values()){
+            if (x.getTipoCuerpo() == CuerpoCeleste.TipoCuerpoCeleste.PLANETA)
+                System.out.println(x.getNombre());
+        }
+
+        // vii.) Obtén del mapa del sistema solar el planeta Marte e imprime todas sus lunas.
+        sistemaSolar.get("Marte").getSatelites()
     }
 }
